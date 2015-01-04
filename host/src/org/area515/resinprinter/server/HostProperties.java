9e51e79c641eff9d63a37a3af5d3177b3e02f5e5
wgilster <wesgilster@gmail.com>
2015-01-04 14:37:09 -0600

    Fixed an enormous amount of bugs added multiprinter capabilities Simplified code Added build process Added script to allow running on Linux
    Former-commit-id: 552130a2b0645aff10e787fa8a1898f824c3c00e

--- host/src/org/area515/resinprinter/server/HostProperties.java
+++ host/src/org/area515/resinprinter/server/HostProperties.java
@@ -3,59 +3,83 @@ package org.area515.resinprinter.server;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 
 public class HostProperties {
-
-	private static HostProperties m_instance = null;
-	public static HostProperties Instance() throws IOException {
-		if (m_instance == null) {
-			m_instance = new HostProperties();
+	private static HostProperties INSTANCE = null;
+	private File uploadDir;
+	private File printDir;
+	private boolean fakeSerial = false;
+	
+	public synchronized static HostProperties Instance() {
+		if (INSTANCE == null) {
+			INSTANCE = new HostProperties();
 		}
-		return m_instance;
+		return INSTANCE;
 	}
 	
-	public HostProperties() throws IOException{HostProperties.init();}
-	
-	static String uploadDir = "";
-	public static String getUploadDir(){
-		return uploadDir;
-	}
-	static String printDir = "";
-	public static String getWorkingDir(){
-		return printDir;
-	}
-	
-	static boolean fakeSerial = false;
-	public static boolean getFakeSerial(){return fakeSerial;}
-	
-	public static void init() throws IOException{
-//		workingDir = HostProperties.getHostProperties().getProperty("printdir");		
-//		sourceDir = HostProperties.getHostProperties().getProperty("sourcedir");
-//		fakeSerial = Boolean.parseBoolean(HostProperties.getHostProperties().getProperty("fakeserial"));
-		// Webapp war plugin doesn't cant find my config.properties - hardcoding for now
-//		workingDir = "/home/user/CreationWorkshop/Host/workingdir";
-//		sourceDir = "/home/user/CreationWorkshop/Host/sourcedir";
-//		fakeSerial = true;
+	public HostProperties() {
+		String printDirString = null;
+		String uploadDirString = null;
 		
-		printDir = "C:\\nonsync\\personal\\ResinPrinterHost\\printdir";
-		uploadDir = "C:\\nonsync\\personal\\ResinPrinterHost\\uploads";
-		fakeSerial = true;
+		InputStream stream = HostProperties.class.getClassLoader().getResourceAsStream("config.properties");
+		if (stream != null) {
+			Properties props = new Properties();
+			try {
+				props.load(stream);
+			} catch (IOException e) {
+				throw new IllegalArgumentException("Couldn't load config.properties file", e);
+			}
+
+			printDirString = props.getProperty("printdir");
+			uploadDirString = props.getProperty("uploaddir");
+			fakeSerial = new Boolean(props.getProperty("fakeserial", "false"));
+		}
 		
-		if(!new File(printDir).exists()){FileUtils.forceMkdir(new File(printDir));}
-		if(!new File(uploadDir).exists()){FileUtils.forceMkdir(new File(uploadDir));}
+		if (printDirString == null) {
+			printDir = new File(System.getProperty("java.io.tmpdir"), "printdir");
+		} else {
+			printDir = new File(printDirString);
+		}
+		
+		if (uploadDirString == null) {
+			uploadDir = new File(System.getProperty("java.io.tmpdir"), "uploaddir");
+		} else {
+			uploadDir = new File(uploadDirString);
+		}
+		
+		if(!printDir.exists()){
+			try {
+				FileUtils.forceMkdir(printDir);
+			} catch (IOException e) {
+				throw new IllegalArgumentException("Couldn't create print directory", e);
+			}
+		}
+		
+		if(!uploadDir.exists()){
+			try {
+				FileUtils.forceMkdir(uploadDir);
+			} catch (IOException e) {
+				throw new IllegalArgumentException("Couldn't create upload directory", e);
+			}
+		}
 		
 		System.out.println("WorkingDir: " + printDir);
 		System.out.println("SourceDir: " + uploadDir);
 		System.out.println("FakeSerial: " + fakeSerial);
 	}
-	
-	public static Properties getHostProperties() throws IOException{
-		Properties properties = new Properties();
-		properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties"));
-		return properties;
+
+	public File getUploadDir(){
+		return uploadDir;
+	}
+	public File getWorkingDir(){
+		return printDir;
 	}
 	
+	public boolean getFakeSerial(){
+		return fakeSerial;
+	}
 }
