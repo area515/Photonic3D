51b29a67bead671db4539bd2f45a9baab6ffa72e
wgilster <wesgilster@gmail.com>
2015-01-10 17:02:01 -0600

    cleannetworkinstall.sh was being generated incorrectly

--- host/cleannetworkinstall.sh
+++ host/cleannetworkinstall.sh
@@ -5,5 +5,5 @@ mkdir cwh
 cd cwh
 wget https://github.com/WesGilster/Creation-Workshop-Host/raw/master/host/cwh-0.17.zip
 unzip cwh-0.17.zip
-chmod 777 cwh-0.17.zip
+chmod 777 start.sh
 rm cwh-0.17.zip
