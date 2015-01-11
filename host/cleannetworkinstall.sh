7e416325db5701655476d2cb0d55e074c7d89d94
wgilster <wesgilster@gmail.com>
2015-01-11 03:51:49 -0600

    Removed manual read timeout
    Former-commit-id: d4a0bd2bd86805290687d6ab18c286ac5bf89b05

--- host/cleannetworkinstall.sh
+++ host/cleannetworkinstall.sh
@@ -3,8 +3,8 @@
 rm -r cwh
 mkdir cwh
 cd cwh
-wget https://github.com/WesGilster/Creation-Workshop-Host/raw/master/host/cwh-0.19.zip
-unzip cwh-0.19.zip
+wget https://github.com/WesGilster/Creation-Workshop-Host/raw/master/host/cwh-0.20.zip
+unzip cwh-0.20.zip
 chmod 777 start.sh
 chmod 777 debug.sh
-rm cwh-0.19.zip
+rm cwh-0.20.zip
