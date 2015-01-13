5f7a882fb7d10f77904f1be35e269ae7eab7b680
wgilster <wesgilster@gmail.com>
2015-01-13 17:35:04 -0600

    fix nullpointer exception in welcome chitchat code

--- host/networkinstall.sh
+++ host/networkinstall.sh
@@ -3,8 +3,8 @@
 rm -r cwh
 mkdir cwh
 cd cwh
-wget https://github.com/WesGilster/Creation-Workshop-Host/raw/master/host/cwh-0.32.zip
-unzip cwh-0.32.zip
+wget https://github.com/WesGilster/Creation-Workshop-Host/raw/master/host/cwh-0.33.zip
+unzip cwh-0.33.zip
 chmod 777 start.sh
 chmod 777 debug.sh
-rm cwh-0.32.zip
+rm cwh-0.33.zip
