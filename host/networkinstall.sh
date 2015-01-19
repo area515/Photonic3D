83e74b0c9c7bbabe8f7fb19a4c5463f86d3e98f5
wgilster <wesgilster@gmail.com>
2015-01-19 14:39:32 -0600

    I believe we are spending too much time in the read method and rxtx is attempting to sync the buffer on the native side and can't get a chance.
    Now we'll try using an event based approach to keep out of the read
    method.

--- host/networkinstall.sh
+++ host/networkinstall.sh
@@ -3,9 +3,9 @@
 rm -r cwh
 mkdir cwh
 cd cwh
-wget https://github.com/WesGilster/Creation-Workshop-Host/raw/master/host/cwh-0.39.zip
-unzip cwh-0.39.zip
+wget https://github.com/WesGilster/Creation-Workshop-Host/raw/master/host/cwh-0.40.zip
+unzip cwh-0.40.zip
 chmod 777 start.sh
 chmod 777 debug.sh
 chmod 777 browseprinter.sh
-rm cwh-0.39.zip
+rm cwh-0.40.zip
