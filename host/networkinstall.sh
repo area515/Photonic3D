c6e3bf4cd17216afc16e9957852a3348aec4fb4a
ergobot <stobryan@gmail.com>
2015-01-18 20:58:33 -0600

    changed path of build to this repo
    Former-commit-id: 214c0d00734bad8663e087407d701621491dad97

--- host/networkinstall.sh
+++ host/networkinstall.sh
@@ -3,7 +3,7 @@
 rm -r cwh
 mkdir cwh
 cd cwh
-wget https://github.com/WesGilster/Creation-Workshop-Host/raw/master/host/cwh-0.34.zip
+wget https://github.com/area515/Creation-Workshop-Host/raw/master/host/cwh-0.34.zip
 unzip cwh-0.34.zip
 chmod 777 start.sh
 chmod 777 debug.sh
