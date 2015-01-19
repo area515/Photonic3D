30e39ac61a3f010991b66e6c3935717be8104ce3
wgilster <wesgilster@gmail.com>
2015-01-19 14:18:54 -0600

    Fixed delay bug and introduced base class for RXTX implementations
    Former-commit-id: 2d8302b98c76fd02cc643ae7d15419545236cc72

--- host/networkinstall.sh
+++ host/networkinstall.sh
@@ -3,9 +3,9 @@
 rm -r cwh
 mkdir cwh
 cd cwh
-wget https://github.com/WesGilster/Creation-Workshop-Host/raw/master/host/cwh-0.37.zip
-unzip cwh-0.37.zip
+wget https://github.com/WesGilster/Creation-Workshop-Host/raw/master/host/cwh-0.38.zip
+unzip cwh-0.38.zip
 chmod 777 start.sh
 chmod 777 debug.sh
 chmod 777 browseprinter.sh
-rm cwh-0.37.zip
+rm cwh-0.38.zip
