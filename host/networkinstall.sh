eeb72a689df1e57f1816aafd1438cdfd8663aac9
wgilster <wesgilster@gmail.com>
2015-01-20 12:07:56 -0600

    Github didn't take my last commit
    Former-commit-id: e9c5970505f093e739d5b3406b7c4f6ba1f46d2e

--- host/networkinstall.sh
+++ host/networkinstall.sh
@@ -3,9 +3,9 @@
 rm -r cwh
 mkdir cwh
 cd cwh
-wget https://github.com/WesGilster/Creation-Workshop-Host/raw/master/host/cwh-0.50.zip
-unzip cwh-0.50.zip
+wget https://github.com/WesGilster/Creation-Workshop-Host/raw/master/host/cwh-0.51.zip
+unzip cwh-0.51.zip
 chmod 777 start.sh
 chmod 777 debug.sh
 chmod 777 browseprinter.sh
-rm cwh-0.50.zip
+rm cwh-0.51.zip
