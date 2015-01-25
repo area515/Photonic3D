fb6d5d61b4fec3dbc24563ddaa1d489840669bc0
wgilster <wesgilster@gmail.com>
2015-01-25 12:25:17 -0600

    Installation on Raspberry Pi should be working
    Former-commit-id: 0acfc70cbeb063c90b333efaaacbaf71f409b2ea

--- host/bin/start.sh
+++ host/bin/start.sh
@@ -2,4 +2,88 @@
 
 cpu=`uname -m`
 
+repo=WesGilster
+
+installDirectory=/opt/cwh
+#It's pretty hard to keep these updated, let me know when they get too old
+if [ "${cpu}" = "armv6l" ]; then 
+	javaURL="http://download.oracle.com/otn-pub/java/jdk/8u33-b05/jdk-8u33-linux-arm-vfp-hflt.tar.gz"
+elif [ "${cpu}" = "i686" ]; then 
+	javaURL="http://download.oracle.com/otn-pub/java/jdk/8u31-b13/jdk-8u31-linux-i586.tar.gz"
+elif [ "${cpu}" = "x86_64" ]; then 
+	javaURL="http://download.oracle.com/otn-pub/java/jdk/8u31-b13/jdk-8u31-linux-x64.tar.gz"
+fi
+
+if [ -f "/usr/lib/jni/librxtxSerial.so" ]; then
+	echo Installing RxTx
+	apt-get install --yes --force-yes librxtx-java
+fi
+
+if [ "${DISPLAY}" = "" ]; then
+	echo DISPLAY variable not set, setting to :0.0
+	DISPLAY=:0.0
+fi;
+
+echo Is the correct version of java installed?
+javaInstalled=`which java`
+javaMajorVersion=`java -version 2>&1 | awk -F[\".] 'NR==1{print "0"$2}'`
+javaMinorVersion=`java -version 2>&1 | awk -F[\".] 'NR==1{print "0"$3}'`
+if [ "$javaInstalled" = "" -o \( $javaMinorVersion -lt 8 -a $javaMajorVersion -le 1 \) ]; then
+	downloadJavaFile=`echo ${javaURL} | awk -F/ '{print $(NF)}'`
+	echo Incorrect version of Java installed, I'll try to install it from this URL: ${javaURL}
+	mkdir -p /usr/lib/jvm
+	cd /usr/lib/jvm
+	rm ${downloadJavaFile}
+	wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "${javaURL}"
+	
+	firstSnapshot=`ls -1`
+	tar xzf ${downloadJavaFile}
+	secondSnapshot=`ls -1`
+	
+	javaInstallFile=`echo "$firstSnapshot"$'\n'"$secondSnapshot" | sort | uniq -u`
+	
+	if [ "${javaInstallFile}" = ""]; then
+		echo "A new version of Java is available, please update this script with the proper download URLS from: http://www.oracle.com/technetwork/java/javase/downloads/index.html"
+		exit
+	fi;
+	
+	echo ${javaInstallFile}
+	#ln -sf /usr/lib/jvm/${javaInstallFile}/bin/java /usr/bin/java
+	#ln -sf /usr/lib/jvm/${javaInstallFile}/bin/javac /usr/bin/javac
+	#ln -sf /usr/lib/jvm/${javaInstallFile}/bin/keytool /usr/bin/keytool		
+	#rm ${downloadJavaFile}
+fi
+
+mkdir -p ${installDirectory}
+cd ${installDirectory}
+mv build.number currentbuildnumber
+wget https://github.com/${repo}/Creation-Workshop-Host/raw/master/host/build.number
+mv build.number networkbuildnumber
+
+currentBuildNumber=`grep build.number currentbuildnumber | awk -F= '{print $2}';`
+networkBuildNumber=`grep build.number networkbuildnumber | awk -F= '{print $2}';`
+
+if [ $networkBuildNumber -gt $currentBuildNumber ]; then
+	echo Installing latest version of cwh: ${networkBuildNumber}
+	rm -r ${installDirectory}
+	mkdir -p ${installDirectory}
+	cd ${installDirectory}
+	wget https://github.com/${repo}/Creation-Workshop-Host/raw/master/host/cwh-0.${networkBuildNumber}.zip
+	unzip cwh-0.${networkBuildNumber}.zip
+	chmod 777 start.sh
+	chmod 777 stop.sh
+	chmod 777 debug.sh
+	chmod 777 browseprinter.sh
+	rm cwh-0.${networkBuildNumber}.zip
+    mv networkbuildnumber build.number
+else
+	rm networkbuildnumber
+	mv currentbuildnumber build.number
+fi
+
+if [ -f "/etc/init.d/cwh" ]; then
+	echo Installing CWH as a service
+	cp ${installDirectory}/cwh /etc/init.d/
+fi
+
 java -Djava.library.path=os/Linux/${cpu} -cp lib/*:. org.area515.resinprinter.server.Main &
---
+++ host/bin/stop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pkill org.area515.resinprinter.server.Main
