fb7654249478f02366838eea80780e4f98467db7
wgilster <wesgilster@gmail.com>
2015-01-25 16:28:37 -0600

    fixed quotes

--- host/bin/start.sh
+++ host/bin/start.sh
@@ -29,8 +29,8 @@ if [ "$javaInstalled" = "" ]; then
 	javaMajorVersion=0
 	javaMinorVersion=0
 else
-	javaMajorVersion=`java -version 2>&1 | grep java version | awk -F[\".] '{print "0"$2}'`
-	javaMinorVersion=`java -version 2>&1 | grep java version | awk -F[\".] '{print "0"$3}'`
+	javaMajorVersion=`java -version 2>&1 | grep "java version" | awk -F[\".] '{print "0"$2}'`
+	javaMinorVersion=`java -version 2>&1 | grep "java version" | awk -F[\".] '{print "0"$3}'`
 fi
 
 if [ "$javaMinorVersion" -lt 8 -a "$javaMajorVersion" -le 1 ]; then
