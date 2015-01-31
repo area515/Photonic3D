127d36ec8b5ce4e55958feecb0d17a35472b6d07
wgilster <wesgilster@gmail.com>
2015-01-31 16:06:09 -0600

    Added common problems section of installnotes. Fixed Display http500 error for good.
    Former-commit-id: d7438d759a1f44d7ff90f6f843a039722635d1cb

--- host/src/org/area515/resinprinter/display/DisplayManager.java
+++ host/src/org/area515/resinprinter/display/DisplayManager.java
@@ -37,13 +37,12 @@ public class DisplayManager {
 	}
 	
 	private DisplayManager(){
-		ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 	}
 
 	public void assignDisplay(Printer newPrinter, GraphicsDevice device) throws AlreadyAssignedException, InappropriateDeviceException {
 		if (device.getIDstring().equals(LAST_AVAILABLE_DISPLAY)) {
 			ArrayList<GraphicsDevice> devices = new ArrayList<GraphicsDevice>();
-			devices.addAll(Arrays.asList(ge.getScreenDevices()));
+			devices.addAll(Arrays.asList(getGraphicsEnvironment().getScreenDevices()));
 			Collections.reverse(devices);
 			for (GraphicsDevice currentDevice : devices) {
 				if (!printersByDisplayIDString.containsKey(currentDevice.getIDstring())) {
@@ -99,9 +98,9 @@ public class DisplayManager {
 	public List<GraphicsDevice> getDisplayDevices() {
 		List<GraphicsDevice> devices = new ArrayList<GraphicsDevice>();
 		try {
-			devices.addAll(Arrays.asList(ge.getScreenDevices()));
-		} catch (HeadlessException | AWTError error) {
-			error.printStackTrace();
+			devices.addAll(Arrays.asList(getGraphicsEnvironment().getScreenDevices()));
+		} catch (InappropriateDeviceException e) {
+			e.printStackTrace();
 		}
 		
 		devices.add(new CustomNamedDisplayDevice(LAST_AVAILABLE_DISPLAY));
@@ -124,7 +123,21 @@ public class DisplayManager {
 	}	
 	
 	public GraphicsDevice getDisplayDevice(int index) throws InappropriateDeviceException {
-		return ge.getScreenDevices()[index];
+		return getGraphicsEnvironment().getScreenDevices()[index];
+	}
+	
+	private GraphicsEnvironment getGraphicsEnvironment() throws InappropriateDeviceException {
+		if (ge != null) {
+			return ge;
+		}
+		
+		try {
+			ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		} catch (HeadlessException | AWTError error) {
+			throw new InappropriateDeviceException("It doesn't look like your graphics environment is properly setup", error);
+		}
+
+		return ge;
 	}
 	
 	public void removeAssignment(Printer printer){
