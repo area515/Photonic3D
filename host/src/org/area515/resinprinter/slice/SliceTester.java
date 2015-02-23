d01a82e1c0036a652efea892810a3e6b8d017118
wgilster <wesgilster@gmail.com>
2015-02-23 07:52:11 -0600

    Started on FillOut/digout determination
    Former-commit-id: dc1e8a8f3a771fb66b4dd483354639a242e64f54

--- host/src/org/area515/resinprinter/slice/SliceTester.java
+++ host/src/org/area515/resinprinter/slice/SliceTester.java
@@ -1,19 +1,23 @@
 package org.area515.resinprinter.slice;
 
 import java.io.FileNotFoundException;
+import java.util.List;
+
+import org.area515.resinprinter.stl.Line3d;
 
 public class SliceTester {
 	public static void main(String[] args) throws FileNotFoundException {
 		 int precisionScaler = 100000;//We need to scale the whole stl large enough to have enough precision before the decimal point
-		 double pixelsPerMMX = 10;
-		 double pixelsPerMMY = 10;
+		 double pixelsPerMMX = 5;
+		 double pixelsPerMMY = 5;
 		 double imageOffsetX = 35 * pixelsPerMMX;
 		 double imageOffsetY = 25 * pixelsPerMMY;
 		 double sliceResolution = 0.1;
 		 
 		 final ZSlicer slicer = new ZSlicer(
-				 //"C:\\Users\\wgilster\\Documents\\ArduinoMega.stl
-				 "C:\\Users\\wgilster\\Documents\\Olaf_set3_whole.stl", 
+				 "C:\\Users\\wgilster\\Documents\\Fat_Guy_Statue.stl",
+				 //"C:\\Users\\wgilster\\Documents\\ArduinoMega.stl",
+				 //"C:\\Users\\wgilster\\Documents\\Olaf_set3_whole.stl", 
 				 precisionScaler, 
 				 pixelsPerMMX, 
 				 pixelsPerMMY, 
@@ -24,9 +28,16 @@ public class SliceTester {
 		 
 		 for (int z = slicer.getZMin(); z < slicer.getZMax(); z++) {
 			 slicer.setZ(z);
+			 System.out.println("Testing Z:" + z);
 			 slicer.colorizePolygons();
 			 if (slicer.getBrokenLoops().size() > 0) {
-				 System.out.println("ERROR:" + z);
+				 for (List<Line3d> lines : slicer.getBrokenLoops()) {
+					 for (Line3d line : lines) {
+						 if (!line.getPointOne().ceilingEquals(line.getPointTwo())) {
+							 System.out.println("Z: " + z + " Non point based broken loop:" + line);
+						 }
+					 }
+				 }
 			 }
 		 }
 	}
---
+++ host/src/org/area515/resinprinter/stl/BrokenFace3d.java
@@ -0,0 +1,22 @@
+package org.area515.resinprinter.stl;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BrokenFace3d implements Face3d {
+	private List<Line3d> lines;
+	private Point3d[] brokenEnds;
+	
+	public BrokenFace3d(List<Line3d> lines) {
+		this.lines = lines;
+		brokenEnds = new Point3d[]{lines.get(0).getPointOne(), lines.get(lines.size() - 1).getPointTwo()};
+	}
+	
+	public Point3d[] getBrokenEnds() {
+		return brokenEnds;
+	}
+	
+	public List<Line3d> getLines() {
+		return lines;
+	}
+}
