ab2de1d5f0cdd8e2f4bcbd29bb196899fd8774f0
wgilster <wesgilster@gmail.com>
2015-09-27 19:53:31 -0500

    Stopped using piped streams since they deadlock on a single thread Stopped using Apache IOUtils.copy since it uses piped streams and have the same issue.

--- host/tests/org/area515/resinprinter/services/MediaUsage.java
+++ host/tests/org/area515/resinprinter/services/MediaUsage.java
@@ -1,6 +1,8 @@
 package org.area515.resinprinter.services;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
@@ -11,15 +13,16 @@ import javax.ws.rs.core.StreamingOutput;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.google.common.io.ByteStreams;
+
 public class MediaUsage {
 	@Test
 	public void tookValidPicture() throws IOException {
 		StreamingOutput output = MediaService.INSTANCE.takePicture("Unknown", 100, 100);
-		PipedInputStream inputStream = new PipedInputStream();
-		PipedOutputStream pipedOutputStream;
-		pipedOutputStream = new PipedOutputStream(inputStream);
-		output.write(pipedOutputStream);
-		BufferedImage image = ImageIO.read(inputStream);
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		output.write(outputStream);
+		
+		BufferedImage image = ImageIO.read(new ByteArrayInputStream(outputStream.toByteArray()));
 		Assert.assertNotNull(image);
 		System.out.println("Picture taken successfully.");
 	}
