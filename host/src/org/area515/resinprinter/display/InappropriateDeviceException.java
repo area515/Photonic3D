372c3dcfe11980252ffd442d9d9fb43bb08a9f97
ergobot <stobryan@gmail.com>
2015-10-11 23:35:53 -0500

    modify printer configuration
    Former-commit-id: c47a30a14129feb7dc7d21a6737af5a0fea2bc2c

--- host/src/org/area515/resinprinter/display/InappropriateDeviceException.java
+++ host/src/org/area515/resinprinter/display/InappropriateDeviceException.java
@@ -3,6 +3,10 @@ package org.area515.resinprinter.display;
 public class InappropriateDeviceException extends Exception {
 	private static final long serialVersionUID = 4586082875895308024L;
 
+	public InappropriateDeviceException(){
+		super();
+	}
+	
 	public InappropriateDeviceException(String message) {
 		super(message);
 	}
