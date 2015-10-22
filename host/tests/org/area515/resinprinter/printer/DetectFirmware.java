6451770a6e063309ce68d28cffd2c932ce1c474a
wgilster <wesgilster@gmail.com>
2015-10-22 07:31:37 -0500

    cwh-0.157 Doubled timeout delay because we were still having trouble with autodetect code Added failfast behavior for tests and asserts
    Former-commit-id: 7cb41ab08481bda8538d7cffc10957a2b7b2edc4

--- host/tests/org/area515/resinprinter/printer/DetectFirmware.java
+++ host/tests/org/area515/resinprinter/printer/DetectFirmware.java
@@ -7,10 +7,9 @@ import java.util.Collections;
 
 import org.area515.resinprinter.printer.MachineConfig.ComPortSettings;
 import org.area515.resinprinter.serial.JSSCCommPort;
-import org.area515.resinprinter.serial.RXTXEventBasedCommPort;
-import org.area515.resinprinter.serial.RXTXSynchronousReadBasedCommPort;
 import org.area515.resinprinter.serial.SerialCommunicationsPort;
 import org.area515.resinprinter.serial.SerialManager;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class DetectFirmware {
@@ -20,7 +19,7 @@ public class DetectFirmware {
 
 		ComPortSettings newComPortSettings = new ComPortSettings();
 		long[] commonSpeeds = new long[]{9600, 115200};
-		
+		boolean hasFound = false;
 		for (long speed : commonSpeeds) {
 			newComPortSettings.setSpeed(speed);
 			newComPortSettings.setDatabits(8);
@@ -34,8 +33,20 @@ public class DetectFirmware {
 				System.out.println("Port:" + currentIdentifier.getName() + " Baud:" + speed);
 				
 				SerialCommunicationsPort port = new JSSCCommPort();
+				Boolean lastValue = null;
 				for (int t = 0; t < 10; t++) {
-					System.out.println("  " + t + ". JSSCCommPort firmware detection:" + SerialManager.Instance().is3dFirmware(port, newComPortSettings));
+					boolean found = SerialManager.Instance().is3dFirmware(port, newComPortSettings);
+					System.out.println("  " + t + ". JSSCCommPort firmware detection:" + found);
+					if (lastValue == null) {
+						lastValue = found;
+					} else {
+						Assert.assertEquals((boolean)lastValue, (boolean)found);
+					}
+					if (found) {
+						hasFound = true;
+					} else {
+						break;
+					}
 				}
 				/*port = new RXTXEventBasedCommPort();
 				for (int t = 0; t < 10; t++) {
@@ -48,5 +59,7 @@ public class DetectFirmware {
 				}*/
 			}
 		}
+		
+		Assert.assertTrue(hasFound);
 	}
 }
