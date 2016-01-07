f1aa26474de312448e5c876ac2e41bb0133e757c
wgilster <wesgilster@gmail.com>
2016-01-06 23:45:09 -0600

    cwh-0.185 Create "Test" button for Material Detector Make sure that material measurement occurs at a time that is not visually dynamic Polarize distance measurement on getDistanceFromLineMidPointToPoint() on line object Ensure that VisualPrintMaterialDetector doesn't deadlock due to stream piping Ensure that InkDetector is self contained when measurement is initiated Added PausedWithWarning state Give better message to user if STMP send fails. Create service for remainingPrintMaterial Create initial tests for material detector against known percentages
    Former-commit-id: 925d0c07424877e73b2457fd59fcf0e066010dad

--- host/src/org/area515/resinprinter/inkdetection/visual/VisualPrintMaterialDetector.java
+++ host/src/org/area515/resinprinter/inkdetection/visual/VisualPrintMaterialDetector.java
@@ -6,33 +6,82 @@ import java.io.InputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.imageio.ImageIO;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.StreamingOutput;
 
 import org.area515.resinprinter.inkdetection.PrintMaterialDetector;
 import org.area515.resinprinter.printer.Printer;
+import org.area515.resinprinter.server.Main;
 import org.area515.resinprinter.services.MediaService;
 
 public class VisualPrintMaterialDetector implements PrintMaterialDetector {
+	private static final HashMap<Printer, StreamingOutput> buildPictures = new HashMap<>();
+	
+	@Override
+	public void startMeasurement(Printer printer) {
+		buildPictures.put(printer, MediaService.INSTANCE.takePicture(printer.getName(), 100, 100));
+	}
+
 	//TODO: There are a ton of ways to make this lighting fast on a raspberry pi 2 and much faster on a Raspberry pi.
 	//		Consider optimizations from both from a multi-processor standpoint, image tiling(ROI) standpoint and one or two general algorithmic optimizations
 	//      This method is pretty much just conceptual for now, just to show something that works
 	@Override
-	public Float getPercentageOfPrintMaterialRemaining(Printer printer) {
+	public float getPercentageOfPrintMaterialRemaining(Printer printer) throws IOException {
 		//Make sure to use takePicture() method to keep everything synchronized...
-		StreamingOutput output = MediaService.INSTANCE.takePicture(printer.getName(), 100, 100);
+		final StreamingOutput output = buildPictures.get(printer);
 		PipedInputStream inputStream = new PipedInputStream();
-		PipedOutputStream pipedOutputStream;
-		try {
-			pipedOutputStream = new PipedOutputStream(inputStream);
-			output.write(pipedOutputStream);
-			return getPrintMaterialRemaining(inputStream);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
+		final PipedOutputStream pipedOutputStream = new PipedOutputStream(inputStream);
+		Main.GLOBAL_EXECUTOR.submit(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					output.write(pipedOutputStream);
+				} catch (WebApplicationException | IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		return getPrintMaterialRemaining(inputStream);
+	}
+	
+	public float getPrintMaterialRemaining(BufferedImage edgesImage) throws IOException {
+		CircleDetector circleDector = new CircleDetector(8, 5, 50, 1);//TODO: these need to be based off of the image size
+		GenericHoughDetection<Circle> houghCircleDetection = new GenericHoughDetection<Circle>(edgesImage, null, circleDector, 0.50f, 0, false);
+		houghCircleDetection.houghTransform();
+		List<Circle> circles = houghCircleDetection.getShapes();
+		//System.out.println(circles);
+		
+		LineDetector lineDetector = new LineDetector(.005d);
+		GenericHoughDetection<Line> houghLineDetection = new GenericHoughDetection<Line>(edgesImage, null, lineDetector, 0.01f, 0, false);
+		houghLineDetection.houghTransform();
+		List<Line> lines = houghLineDetection.getShapes();
+		//System.out.println(lines);
+		
+		//This assumes the camera is oriented such that +y = direction that gravity pulls objects
+		List<Float> percentages = new ArrayList<Float>();
+		for (Circle currentCircle : circles) {
+			for (Line currentLine : lines) {
+				Line intersectedLine = currentCircle.intersection(currentLine);
+				if (intersectedLine != null) {
+					double distance = intersectedLine.getDistanceFromLineMidPointToPoint(currentCircle.getX(), currentCircle.getY());
+					percentages.add(.5f + (float)(distance / (double)currentCircle.getRadius()) * .5f);
+				}
+			}
 		}
+
+		float total = 0;
+		for (Float aFloat : percentages) {
+			total += aFloat;
+		}
+		
+		return total / percentages.size();
 	}
 	
 	public float getPrintMaterialRemaining(InputStream inputStream) throws IOException {
@@ -45,39 +94,6 @@ public class VisualPrintMaterialDetector implements PrintMaterialDetector {
 		detector.process();
 		
 		BufferedImage edgesImage = detector.getEdgesImage();
-		CircleDetector circleDector = new CircleDetector(8, 10, 50, 1);//TODO: these need to be based off of the image size
-		GenericHoughDetection<Circle> houghCircleDetection = new GenericHoughDetection<Circle>(edgesImage, null, circleDector, 0.50f, 0, false);
-		houghCircleDetection.houghTransform();
-		List<Circle> circles = houghCircleDetection.getShapes();
-		//System.out.println(circles);
-		
-		LineDetector lineDetector = new LineDetector(.005d);
-		GenericHoughDetection<Line> houghLineDetection = new GenericHoughDetection<Line>(edgesImage, null, lineDetector, 0.06f, 0, false);
-		houghLineDetection.houghTransform();
-		List<Line> lines = houghLineDetection.getShapes();
-		//System.out.println(lines);
-		
-		//This assumes the camera is oriented such that +y = direction that gravity pulls objects
-		List<Float> percentages = new ArrayList<Float>();
-		for (Circle currentCircle : circles) {
-			for (Line currentLine : lines) {
-				Line intersectedLine = currentCircle.intersection(currentLine);
-				if (intersectedLine != null) {
-					double distance = intersectedLine.getDistanceFromLineMidPointToPoint(currentCircle.getX(), currentCircle.getY());
-					if (distance < 0) {
-						percentages.add(.5f - (float)(distance / (double)currentCircle.getRadius()) * .5f);
-					} else {
-						percentages.add(.5f + (float)(distance / (double)currentCircle.getRadius()) * .5f);
-					}
-				}
-			}
-		}
-		
-		float total = 0;
-		for (Float aFloat : percentages) {
-			total += aFloat;
-		}
-		
-		return total / percentages.size();
+		return getPrintMaterialRemaining(edgesImage);
 	}
 }
