e1ae801bb3fb9c7eeaccf98e75aad6666e9419fd
wgilster <wesgilster@gmail.com>
2016-01-30 08:28:34 -0600

    cwh-0.212 Install process now takes care of a few more situations: 1. For Raspberry Pi extend disk partition(I could check to see if this is necessary) 2. For Raspberry Pi automatically Enable camera 3. For Raspberry Pi reboot 4. For Raspberry Pi get back into contact with Raspberry Pi 5. For Raspberry Pi fdisk to automatically fill partition 6. Perform normal install as with any Linux box 7. For Raspberry Pi Change password
    Former-commit-id: eaaf28f949a81ae35472dddfcaa3faf4933b7e73

--- host/src/org/area515/resinprinter/client/Main.java
+++ host/src/org/area515/resinprinter/client/Main.java
@@ -4,11 +4,13 @@ import java.awt.BorderLayout;
 import java.awt.Desktop;
 import java.io.IOException;
 import java.net.SocketException;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import javax.swing.JDialog;
@@ -103,31 +105,91 @@ public class Main {
 		}
 	}
 	
-	public static boolean extendDiskPartition(SSHClient client) {
+	public static SSHClient raspberryPiSpecificChanges(SSHClient client, JOptionPane progress) {
+		progress.setMessage("Enabling camera...");
+		client.send("sed /boot/config.txt -i -e \"s/^start_x/#start_x/\"");
+		client.send("sed /boot/config.txt -i -e \"s/^gpu_mem/#gpu_mem/\"");
+		client.send("sed /boot/config.txt -i -e \"s/^startx/#startx/\"");
+		client.send("sed /boot/config.txt -i -e \"s/^fixup_file/#fixup_file/\"");
+		client.send("echo \"\nstart_x=1\ngpu_mem=128\n\" >> /boot/config.txt");
+
+		progress.setMessage("Extending disk partition...");
 		String[] rootPartition = client.send("readlink /dev/root");
 		if (!rootPartition[0].startsWith("mmcblk0p")) {
+			System.out.println("rootPartition:" + Arrays.toString(rootPartition));
 			System.out.println("/dev/root either doesn't exist or isn't an SD card.");
-			return false;
+			return client;
 		}
-		rootPartition[0] = rootPartition[0].substring(8);
+		rootPartition[0] = rootPartition[0].substring(8).trim();
 		if (!rootPartition[0].equals("2")) {
+			System.out.println("rootPartition:" + Arrays.toString(rootPartition));
 			System.out.println("/dev/root either doesn't exist or isn't an SD card.");
-			return false;
+			return client;
 		}
 		String[] lastPartition = client.send("parted /dev/mmcblk0 -ms unit s p | tail -n 1 | cut -f 1 -d:");
+		lastPartition[0] = lastPartition[0].trim();
 		if (!lastPartition[0].equals(rootPartition[0])) {
+			System.out.println("rootPartition:" + Arrays.toString(rootPartition));
+			System.out.println("lastPartition:" + Arrays.toString(lastPartition));
 			System.out.println("/dev/root must be the last partition in order to expand it.");
-			return false;
+			return client;
 		}
 		
 		String[] startOfPartition = client.send("parted /dev/mmcblk0 -ms unit s p | grep \"^" + lastPartition[0] + "\" | cut -f 2 -d:");
-		String[] fdiskReturn = client.send("fdisk /dev/mmcblk0\np\nd\n" + rootPartition[0] + "\nn\np\n" + rootPartition[0] + "\n" + startOfPartition[0] + "\n\np\nw");
-//reboot here...
-		client.send("resize2fs /dev/root");
-		return true;
+		System.out.println("startOfPartition");
+		writeOutput(startOfPartition);
+		startOfPartition[0] = startOfPartition[0].trim();
+		String[] fdiskReturn = client.send("echo \"\np\nd\n" + rootPartition[0] + "\nn\np\n" + rootPartition[0] + "\n" + startOfPartition[0] + "\n\np\nw\n\" | fdisk /dev/mmcblk0");
+		System.out.println("fdiskReturn");
+		writeOutput(fdiskReturn);
+		
+		//Execute this in a different thread because I'm not sure if I can ensure that we will get a prompt back by the time our shell is disconnected by the OS.
+		final SSHClient threadClient = client;
+		long start = System.currentTimeMillis();
+		Executors.newSingleThreadExecutor().submit(new Runnable() {
+			@Override
+			public void run() {
+				threadClient.send("reboot");
+			}
+		});
+
+		try {
+			//We need to sleep long enough for the reboot to execute so we don't immediately connect to a doomed shell.
+			//This is perhaps the first time I've found that a sleep "should" be used to solve a race condition, and I'm
+			//still not very convinced. Maybe I could wait for: "The system is going down for reboot NOW!" to be our 
+			//trigger to attempt a new SSH connection. However, what if we were able to sneak a quick connection on our
+			//existing doomed shell? Sleep doesn't seem too bad now huh?
+			Thread.sleep(3000);
+		} catch (InterruptedException e1) {}
+		progress.setMessage("Restarting device...");
+		client.disconnect();
+		
+		String host = client.getHost();
+		while (System.currentTimeMillis() - start < 30000) {
+			client = new SSHClient();
+			try {
+				client.connect("pi", "raspberry", host, 22);
+				break;
+			} catch (JSchException e) {
+				System.out.println("Reconnect unsuccessful:"  + e.getMessage());
+				client = null;
+			}
+		}
+		
+		System.out.println("Found client:" + client + " after:" + (System.currentTimeMillis() - start));
+		if (client == null) {
+			return null;
+		}
+		
+		progress.setMessage("Finishing partition resize...");
+		System.out.println("sudo required:" + client.sudoIfNotRoot());
+		String[] resizeResponse = client.send("resize2fs /dev/root");
+		System.out.println("resizeResponse");
+		writeOutput(resizeResponse);
+		return client;
 	}
 	
-	public static boolean performInstall(Box box) throws IOException, JSchException  {
+	public static boolean performInstall(Box box, String username, String oldPassword) throws IOException, JSchException  {
 		System.out.println("User chose install on:" + box);
 		
 		final JOptionPane installOptionPane = new JOptionPane("Installing CWH...", JOptionPane.INFORMATION_MESSAGE, JOptionPane.CANCEL_OPTION, null, new String[]{"Cancel"}, "Cancel");
@@ -145,14 +207,19 @@ public class Main {
 		SSHClient client = new SSHClient();
 		try {
 			installOptionPane.setMessage("Connecting to printer...");
-			client.connect("pi", "raspberry", box.getName(), 22);
+			client.connect(username, oldPassword, box.getName(), 22);
 			System.out.println("sudo required:" + client.sudoIfNotRoot());
 			client.send("rm start.sh");
 						
 			//A couple of specialty items if it's a Raspberry Pi
-			/*if (box.isRaspberryPi()) {
-				extendDiskPartition(client);
-			}*/
+			if (box.isRaspberryPi()) {
+				client = raspberryPiSpecificChanges(client, installOptionPane);
+				if (client == null) {
+					installPane.setVisible(false);
+					JOptionPane.showConfirmDialog(null, "Lost contact with the device after attempting to extend the disk partition.", "Installation Failed", JOptionPane.ERROR);
+					return false;
+				}
+			}
 			
 			installOptionPane.setMessage("Downloading installation scripts...");
 			String[] output = client.send("wget https://github.com/area515/Creation-Workshop-Host/raw/master/host/bin/start.sh");
@@ -168,18 +235,21 @@ public class Main {
 				throw new IOException("There was a problem installing CWH. Please refer to logs.");
 			}
 			
-			installPane.setVisible(false);
-			char[] password = getPassword("Please Secure This Device", 
-					"<html>This device was setup with a default password<br>" + 
-					"from the manufacturer. It is not advisable that<br>" + 
-					"you keep this password. Please enter another<br>" + 
-					"password to help secure your printer.<html>");
-			if (password == null) {
-				return true;
+			if (box.isRaspberryPi()) {
+				installPane.setVisible(false);
+				char[] password = getPassword("Please Secure This Device", 
+						"<html>This device was setup with a default password<br>" + 
+						"from the manufacturer. It is not advisable that<br>" + 
+						"you keep this password. Please enter another<br>" + 
+						"password to help secure your printer.<html>");
+				if (password == null) {
+					return true;
+				}
+			
+				output = client.send("echo 'pi:" + new String(password) + "' | chpasswd");
+				//JOptionPane.showMessageDialog(null, "Failed to set password. " + output[0], "Bad Password", JOptionPane.WARNING_MESSAGE);
 			}
 			
-			output = client.send("echo 'pi:" + new String(password) + "' | chpasswd");
-			//JOptionPane.showMessageDialog(null, "Failed to set password. " + output[0], "Bad Password", JOptionPane.WARNING_MESSAGE);
 			return true;
 
 		} finally {
@@ -270,7 +340,7 @@ public class Main {
 						if (box != null) {
 							try {
 								if (box.isRaspberryPi()) {
-									if (!performInstall(box)) {
+									if (!performInstall(box, "pi", "raspberry")) {
 										System.exit(-1);
 									}
 								} else {
