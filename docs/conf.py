20449d1b00613bb19679c204ca41821a20bcbcb1
ergobot <stobryan@gmail.com>
2016-03-21 17:31:07 -0500

    attempting readthedocs
    Former-commit-id: d645abbe92c56839d9b830fe8647a1f8c2b32d64

--- docs/conf.py
+++ docs/conf.py
@@ -47,8 +47,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Photonic3D'
-copyright = u'2016, WesGilster, jmkao, kloknibor, ergobot'
-author = u'WesGilster, jmkao, kloknibor, ergobot'
+copyright = u'Photonic3D'
+author = u'Photonic3D'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
