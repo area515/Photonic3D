9d74d6901a716b6904079f131e024115c20409b1
Wilbur Shi <wilburshi@sbcglobal.net>
2016-06-16 21:55:37 -0700

    Slightly functional flip checkbox connecting with Customizers

--- host/resourcesnew/cwh/js/printables.js
+++ host/resourcesnew/cwh/js/printables.js
@@ -4,26 +4,22 @@
 		controller = this;
 		
 		this.currentPrintable = null;
+		this.currentCustomizer = null;
 		this.supportedFileTypes = null;
-		// Code added by Wilbur Shi
-		this.flipped = {
-			value: false
-		};
 
-		if (this.currentPrintable != null) {
-			// Do flipping stuff
-			this.testName = currentPrintable.name;
-		} else {
-			// Hide preview area in the html (checkbox and preview panel)
-			this.testName = null;
-		}
-		// End code added by Wilbur Shi
 		this.refreshPrintables = function refreshPrintables() {
 			$http.get("/services/printables/list").success(
         		function (data) {
         			controller.printables = data;
         		}
 	        );
+	        // Code added by Wilbur Shi
+	        $http.get("/services/customizers/list").success(
+        		function (data) {
+        			controller.customizers = data;
+        		}
+	        );
+	        // End code added by Wilbur Shi
 		}
 		this.hostSocket = cwhWebSocket.connect("services/hostNotification", $scope).onJsonContent(
 			function(data) {
@@ -35,6 +31,41 @@
 		if (this.hostSocket == null) {
 			$scope.$emit("MachineResponse",  {machineResponse: {command:"Browser Too Old", message:"You will need to use a modern browser to run this application."}});
 		}
+
+		// Code added by Wilbur Shi
+		// this.flipped = {
+		// 	value: false
+		// };
+
+		// if (this.currentPrintable != null) {
+		// 	// Do flipping stuff
+		// 	this.testName = currentPrintable.name;
+		// } else {
+		// 	// Hide preview area in the html (checkbox and preview panel)
+		// 	this.testName = null;  
+		// }
+		this.changeFlip = function changeFlip(flip) {
+			if (controller.currentCustomizer != null) {
+				var affineTransformSettings = controller.currentCustomizer.getAffineTransformSettings();
+				if (flip) {
+					affineTransformSettings.setyScale(-1);
+					controller.changeMsg = "Set yScale to -1";
+				} else {
+					affineTransformSettings.setyScale(0);
+					controller.changeMsg = "Set yScale to 0";
+				}
+			}
+
+			// Since there is no currentCustomizer, here is a placeholder msg: 
+			controller.changeMsg = "CustomizerService is not implemented, so here is a placeholder changeMsg: ";
+			if (flip) {
+				controller.changeMsg += "Set yScale to -1.";
+			} else {
+				controller.changeMsg += "Set yScale to 0.";
+			}
+		}
+		// End code added by Wilbur Shi
+
 		this.printPrintable = function printPrintable() {
 			var printableName = encodeURIComponent(controller.currentPrintable.name);
 			var printableExtension = encodeURIComponent(controller.currentPrintable.extension);
@@ -62,6 +93,13 @@
 	    }
 		this.changeCurrentPrintable = function changeCurrentPrintable(newPrintable) {
 			controller.currentPrintable = newPrintable;
+			// Code added by Wilbur Shi
+			$http.get("/services/customizers/getByPrintableName/" + newPrintable.name).success(
+									function (data) {
+										// Once this method is implemented, change null to data in order to save the correct Customizer.
+										controller.currentCustomizer = null;
+									});
+			// End code added by Wilbur Shi
 		}
 
 		//TODO: When we get an upload complete message, we need to refresh file list...
