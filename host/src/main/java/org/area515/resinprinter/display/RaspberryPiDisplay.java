ef3b702cf948e3db7a5f14467b5c70bbebd2a512
wgilster <wesgilster@gmail.com>
2017-02-13 07:58:17 -0600

    Checked in DispManX api test (unorganized and untested)

--- host/src/main/java/org/area515/resinprinter/display/RaspberryPiDisplay.java
+++ host/src/main/java/org/area515/resinprinter/display/RaspberryPiDisplay.java
@@ -1,14 +1,18 @@
 package org.area515.resinprinter.display;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
 import com.sun.jna.Library;
 import com.sun.jna.Native;
+import com.sun.jna.Structure;
+import com.sun.jna.ptr.IntByReference;
 
 public class RaspberryPiDisplay {
-	public interface DispManX extends Library {
-		DispManX INSTANCE = (DispManX)Native.loadLibrary("bcm_host", DispManX.class);
-	}
 
-	public RaspberryPiDisplay() {
-		DispManX dispMan = DispManX.INSTANCE;
-	}
 }
