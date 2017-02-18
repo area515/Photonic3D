1dc400eae96f22cc77bc832b7e09d669376339fc
wgilster <wesgilster@gmail.com>
2017-02-18 14:51:50 -0600

    DispManXTest working perfectly on touch screen

--- host/src/test/java/org/area515/resinprinter/display/DispManXTest.java
+++ host/src/test/java/org/area515/resinprinter/display/DispManXTest.java
@@ -9,7 +9,9 @@ import java.util.List;
 import javax.imageio.ImageIO;
 
 import com.sun.jna.Library;
+import com.sun.jna.Memory;
 import com.sun.jna.Native;
+import com.sun.jna.Pointer;
 import com.sun.jna.Structure;
 import com.sun.jna.ptr.IntByReference;
 
@@ -17,9 +19,9 @@ public class DispManXTest {
 	public static class VC_DISPMANX_ALPHA_T extends Structure {
 		public static class ByReference extends VC_DISPMANX_ALPHA_T implements Structure.ByReference {}
 		
-		int flags;
-		int opacity;
-		int mask;
+		public int flags;
+		public int opacity;
+		public int mask;
 		
 		@Override
 		protected List<String> getFieldOrder() {
@@ -29,10 +31,10 @@ public class DispManXTest {
 	
 	public static class VC_RECT_T extends Structure {
 		public static class ByReference extends VC_RECT_T implements Structure.ByReference {}
-		   int x;
-		   int y;
-		   int width;
-		   int height;
+		   public int x;
+		   public int y;
+		   public int width;
+		   public int height;
 	   
 	    @Override
 		protected List<String> getFieldOrder() {
@@ -185,29 +187,29 @@ public class DispManXTest {
 		public static final int DISPMANX_PROTECTION_NONE = 0;
 		public static final int DISPMANX_PROTECTION_HDCP = 11;
 		
-		public void bcm_host_init();
+		public int bcm_host_init();
 		public int graphics_get_display_size(int screenIndex, IntByReference width, IntByReference height);
 		public int vc_dispmanx_display_open(int screenIndex);
-		public int vc_dispmanx_resource_create(int resourceType, int width, int height, byte[] imagePointer);
+		public int vc_dispmanx_resource_create(int resourceType, int width, int height, IntByReference imagePointer);
 		public int vc_dispmanx_rect_set(VC_RECT_T.ByReference rectangleToCreate, int offsetX, int offsetY, int width, int height);
-		public int vc_dispmanx_update_start(int iHaveNoIdea);//Constant of 10
+		public int vc_dispmanx_update_start(int priority);//Constant of 10
 		public int vc_dispmanx_element_add(
 				int updateHandle, 
 				int displayHandle,
-				int iHaveNoIdea1,                            // 2000
-				VC_RECT_T.ByReference screenRectangle, 
-				int resourceHandle, 
-				VC_RECT_T.ByReference blitRectangle,
+				int layer,                            // 2000
+				VC_RECT_T.ByReference destinationRectangle, 
+				int sourceResourceHandle, 
+				VC_RECT_T.ByReference sourceRectangle,
 				int protectionMode,                          // DISPMANX_PROTECTION_NONE
 				VC_DISPMANX_ALPHA_T.ByReference alpha,       // { DISPMANX_FLAGS_ALPHA_FROM_SOURCE | DISPMANX_FLAGS_ALPHA_FIXED_ALL_PIXELS, 255, 0 };
-				int iHaveNoIdea2,                            // 0
+				int clamp,                            // 0
 				int imageTransformation);                    // VC_IMAGE_ROT0 
 		public int vc_dispmanx_update_submit_sync(int updateHandle);
 		public int vc_dispmanx_resource_write_data(
 				int resourceHandle,
 				int resourceType,                            // VC_IMAGE_RGB565
 				int pitch, 
-				byte[] imageConvertedFromBufferedImage, 
+				Pointer imageConvertedFromBufferedImage, 
 				VC_RECT_T.ByReference copyRectangle);
 		public int vc_dispmanx_element_remove(int updateHandle, int elementHandle);
 		public int vc_dispmanx_resource_delete(int resourceHandle);
@@ -225,8 +227,8 @@ public class DispManXTest {
         }
 	}
 	
-	public static void usage(String name ) {
-		System.out.printf( "usage: %s <display> <duration in seconds> <file>\n", name );
+	public static void usage() {
+		System.out.printf( "usage: org.area515.resinprinter.display.DispManXTest <display> <duration in seconds> <file>\n");
 		displayInfo( DispManX.DISPMANX_ID_MAIN_LCD, "Main LCD" );
 		displayInfo( DispManX.DISPMANX_ID_AUX_LCD, "AUX LCD" );
 		displayInfo( DispManX.DISPMANX_ID_HDMI, "HDMI" );
@@ -253,76 +255,118 @@ public class DispManXTest {
 		if ( val != 0 ) {
 			System.out.printf( "%s: %08x\n", str, val );
 		} else {
-			System.out.println("val is 0");
+			System.out.println(str + " is 0");
 		}
 	}
 	
-	public static byte[] loadBitmap(String fileName, int width, int height, IntByReference pitch) throws IOException {
+	public static int getPitch( int x, int y ) {
+	        return ((x + (y)-1) & ~((y)-1));//y*((x + y-1)/y);
+	}
+	
+	public static Memory loadBitmapRGB565(String fileName, IntByReference width, IntByReference height, IntByReference pitchByRef) throws IOException {
+		int bytesPerPixel = 2;
 		BufferedImage image = ImageIO.read(new File(fileName));
-		byte[] destPixels = new byte[image.getWidth() * image.getHeight() * 3];
-		int i = 0;
+		int pitch = getPitch( bytesPerPixel * image.getWidth(), 32 );
+		pitchByRef.setValue(pitch);
+		Memory destPixels = new Memory(pitch * image.getHeight());
         for (int y = 0; y < image.getHeight(); y++) {
             for (int x = 0; x < image.getWidth(); x++) {
         		int rgb = image.getRGB(x, y);
-                destPixels[i++] = (byte)((rgb >> 16) & 0xFF);
-                destPixels[i++] = (byte)((rgb >> 8) & 0xFF);
-                destPixels[i++] = (byte)((rgb >> 0) & 0xFF);
+        		destPixels.setShort((y*(pitch / bytesPerPixel) + x) * bytesPerPixel, (short)(((rgb & 0xf80000) >>> 8) | ((rgb & 0xfc00) >>> 5) | (rgb & 0xf8 >>> 3)));
             }
         }
+        width.setValue(image.getWidth());
+        height.setValue(image.getHeight());
         return destPixels;
 	}
 	
+	public static Memory loadBitmapARGB8888(String fileName, IntByReference width, IntByReference height, IntByReference pitchByRef) throws IOException {
+		int bytesPerPixel = 4;
+		BufferedImage image = ImageIO.read(new File(fileName));
+		int pitch = getPitch( bytesPerPixel * image.getWidth(), 32 );
+		pitchByRef.setValue(pitch);
+		long start = System.currentTimeMillis();
+		Memory destPixels = new Memory(pitch * image.getHeight());
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+        		destPixels.setInt((y*(pitch / bytesPerPixel) + x) * bytesPerPixel, image.getRGB(x, y));
+            }
+        }
+        System.out.println(System.currentTimeMillis() - start);
+        width.setValue(image.getWidth());
+        height.setValue(image.getHeight());
+        return destPixels;
+	}
+	
+	//sudo vi DispManXTest.java
+	//sudo javac -cp lib/*:. DispManXTest.java
+	//sudo java -cp lib/*:. DispManXTest 2 10 resourcesnew/favicon/apple-icon-144x144.png
 	public static void main(String[] args) throws IOException, InterruptedException {
 		DispManX dispMan = DispManX.INSTANCE;
-		dispMan.bcm_host_init();
-        if ( args.length < 2 ) {
-        	usage( args[0] );
+		System.out.println("Initialized:" + dispMan.bcm_host_init());
+        if ( args.length < 3 ) {
+        	usage();
+        	return;
         }
-
-        int screen = Integer.parseInt( args[1] );
-        if ( args.length == 2 ) {
-        	info(screen);
-        }
-
-        int time = Integer.parseInt( args[2] );
+        
+        int screen = Integer.parseInt( args[0] );
+        int time = Integer.parseInt( args[1] );
         IntByReference pitch = new IntByReference();
         IntByReference width = new IntByReference();
         IntByReference height = new IntByReference();
         res( "get display size", dispMan.graphics_get_display_size( screen, width, height ) );
-        System.out.printf( "display %d: %d x %d\n", screen, width, height );
+        System.out.printf( "display %d: %d x %d\n", screen, width.getValue(), height.getValue() );
         int display = dispMan.vc_dispmanx_display_open( screen );
-        byte[] bitmap = loadBitmap( args[3], width.getValue(), height.getValue(), pitch );
+        Memory bitmap = loadBitmapARGB8888( args[2], width, height, pitch );
+        System.out.printf( "bitmap: %d x %d pitch->%d\n", width.getValue(), height.getValue(), pitch.getValue());
 
-        // create a texture
-        int texture = dispMan.vc_dispmanx_resource_create( VC_IMAGE_TYPE_T.VC_IMAGE_RGB565.getcIndex(), width.getValue(), height.getValue(), bitmap );
-
+        IntByReference ref = new IntByReference();
+        int resourceHandle = dispMan.vc_dispmanx_resource_create( VC_IMAGE_TYPE_T.VC_IMAGE_ARGB8888.getcIndex(), width.getValue(), height.getValue(), ref );
+        
         VC_RECT_T.ByReference copyRect = new VC_RECT_T.ByReference();
-        VC_RECT_T.ByReference blitRect = new VC_RECT_T.ByReference();
-        VC_RECT_T.ByReference screenRect = new VC_RECT_T.ByReference();
+        VC_RECT_T.ByReference sourceRect = new VC_RECT_T.ByReference();
+        VC_RECT_T.ByReference destinationRect = new VC_RECT_T.ByReference();
+        
         res( "rect set", dispMan.vc_dispmanx_rect_set( copyRect, 0, 0, width.getValue(), height.getValue() ) );
-        res( "rect set", dispMan.vc_dispmanx_rect_set( blitRect, 0, 0, width.getValue()<<16, height.getValue()<<16 ) );
-        res( "rect set", dispMan.vc_dispmanx_rect_set( screenRect, 0, 0, width.getValue(), height.getValue() ) );
+        //This seems to be some form of a zoom factor
+        res( "rect set", dispMan.vc_dispmanx_rect_set( sourceRect, 0, 0, width.getValue()<<16, height.getValue()<<16 ) );
+        res( "rect set", dispMan.vc_dispmanx_rect_set( destinationRect, 0, 0, width.getValue(), height.getValue() ) );
 
-        int update = dispMan.vc_dispmanx_update_start( 10 );
+        System.out.println("copyRect:" + copyRect.width + ", " + copyRect.height);
+        System.out.println("sourceRect:" + sourceRect.width + ", " + sourceRect.height);
+        System.out.println("destinationRect:" + destinationRect.width + ", " + destinationRect.height);
+        
+        int update = dispMan.vc_dispmanx_update_start( 0 );
         VC_DISPMANX_ALPHA_T.ByReference alpha = new VC_DISPMANX_ALPHA_T.ByReference();
         alpha.flags = ALPHA.DISPMANX_FLAGS_ALPHA_FROM_SOURCE.getFlag() | ALPHA.DISPMANX_FLAGS_ALPHA_FIXED_ALL_PIXELS.getFlag();
         alpha.opacity = 255;
-        int element = dispMan.vc_dispmanx_element_add( update, display, 2000, screenRect, texture, blitRect, DispManX.DISPMANX_PROTECTION_NONE, alpha, 0, VC_IMAGE_TRANSFORM_T.VC_IMAGE_ROT0.getcConst() );
+        int element = dispMan.vc_dispmanx_element_add( 
+        		update, 
+        		display, 
+        		1, 
+        		destinationRect, 
+        		resourceHandle, 
+        		sourceRect, 
+        		DispManX.DISPMANX_PROTECTION_NONE, 
+        		alpha, 
+        		0, 
+        		VC_IMAGE_TRANSFORM_T.VC_IMAGE_ROT0.getcConst() );
         res( "submit", dispMan.vc_dispmanx_update_submit_sync( update ) );
         
         res( "resource write data", dispMan.vc_dispmanx_resource_write_data( 
-        		texture, 
-        		VC_IMAGE_TYPE_T.VC_IMAGE_RGB565.getcIndex(), 
+        		resourceHandle, 
+        		VC_IMAGE_TYPE_T.VC_IMAGE_ARGB8888.getcIndex(), 
         		pitch.getValue() , 
-        		bitmap, copyRect )
+        		bitmap, 
+        		destinationRect )
         	);
 
-        Thread.sleep( time );
-        update = dispMan.vc_dispmanx_update_start( 10 );
+        Thread.sleep( time * 1000 );
+        update = dispMan.vc_dispmanx_update_start( 0 );
 
         res( "element remove", dispMan.vc_dispmanx_element_remove( update, element ) );
         res( "submit", dispMan.vc_dispmanx_update_submit_sync( update ) );
-        res( "resource delete", dispMan.vc_dispmanx_resource_delete( texture ) );
+        res( "resource delete", dispMan.vc_dispmanx_resource_delete( resourceHandle ) );
         res( "display close", dispMan.vc_dispmanx_display_close( display ) );
 	}
 }
