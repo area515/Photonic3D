b62a363fcf18977a829b98e414f47648b651bf7d
wgilster <wesgilster@gmail.com>
2017-02-26 13:55:15 -0600

    For as simple of a class this is, it actually causes some pretty significant issues

--- host/src/main/java/org/area515/util/Log4jTimer.java
+++ host/src/main/java/org/area515/util/Log4jTimer.java
@@ -7,53 +7,64 @@ import org.apache.logging.log4j.ThreadContext;
 
 public class Log4jTimer {
 	public static Map<String, String> GLOBAL = new HashMap<>();
-	
-	private static long startTimer(Map<String, String> map, String timerName) {
+
+	public static long startTimer(String timerName) {
 		long newTime = System.currentTimeMillis();
-		map.put(timerName, newTime + "");
+		ThreadContext.put(timerName, newTime + "");
 		return newTime;
 	}
 	
-	public static long splitTimer(Map<String, String> map, String timerName) {
+	public static long startGlobalTimer(String timerName) {
 		long newTime = System.currentTimeMillis();
-		long timeTaken = newTime - Long.parseLong(map.get(timerName));
-		map.put(timerName, newTime + "");
+		GLOBAL.put(timerName, newTime + "");
+		return newTime;
+	}
+
+	public static long splitTimer(String timerName) {
+		long newTime = System.currentTimeMillis();
+		String value = ThreadContext.get(timerName);
+		if (value == null) {
+			return startTimer(timerName);
+		}
+
+		long timeTaken = newTime - Long.parseLong(value);
+		ThreadContext.put(timerName, newTime + "");
 		return timeTaken;
 	}
 
-	public static long completeTimer(Map<String, String> map, String timerName) {
+	public static long splitGlobalTimer(String timerName) {
+		long newTime = System.currentTimeMillis();		
+		String value = GLOBAL.get(timerName);
+		if (value == null) {
+			return startGlobalTimer(timerName);
+		}
+
+		long timeTaken = newTime - Long.parseLong(value);
+		GLOBAL.put(timerName, newTime + "");
+		return timeTaken;
+	}
+	
+	public static long completeTimer(String timerName) {
 		long newTime = System.currentTimeMillis();
-		String value = map.get(timerName);
+		String value = ThreadContext.get(timerName);
 		if (value == null) {
 			return -1;
 		}
 		
 		long timeTaken = newTime - Long.parseLong(value);
-		map.remove(timerName);
+		ThreadContext.remove(timerName);
 		return timeTaken;
-	}
-
-	public static long startTimer(String timerName) {
-		return startTimer(ThreadContext.getContext(), timerName);
-	}
-	
-	public static long startGlobalTimer(String timerName) {
-		return startTimer(GLOBAL, timerName);
-	}
-
-	public static long splitTimer(String timerName) {
-		return splitTimer(ThreadContext.getContext(), timerName);
-	}
-
-	public static long splitGlobalTimer(String timerName) {
-		return splitTimer(GLOBAL, timerName);
-	}
-	
-	public static long completeTimer(String timerName) {
-		return completeTimer(ThreadContext.getContext(), timerName);
 	}
 	
 	public static long completeGlobalTimer(String timerName) {
-		return completeTimer(GLOBAL, timerName);
+		long newTime = System.currentTimeMillis();
+		String value = GLOBAL.get(timerName);
+		if (value == null) {
+			return -1;
+		}
+		
+		long timeTaken = newTime - Long.parseLong(value);
+		GLOBAL.remove(timerName);
+		return timeTaken;
 	}
 }
