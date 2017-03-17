3034c8e2d68e1e6755965244334a827e5f4e9f89
wgilster <wesgilster@gmail.com>
2017-03-17 08:05:35 -0500

    Fixed submit on enter

--- host/resourcesnew/cwh/js/index.js
+++ host/resourcesnew/cwh/js/index.js
@@ -7,6 +7,20 @@
 		    };
 		}]);
 		
+		cwhApp.directive('onEnter', function () {
+		    return function (scope, element, attrs) {
+		        element.bind("keydown keypress", function (event) {
+		            if(event.which === 13) {
+		                scope.$apply(function (){
+		                    scope.$eval(attrs.onEnter);
+		                });
+
+		                event.preventDefault();
+		            }
+		        });
+		    };
+		});
+		
 		cwhApp.factory('photonicUtils', ['$http', '$rootScope', function($http, $rootScope) {
 	        return {
 	        	previewExternalStateId:firstCacheId,
