b67c3b1726a2434e80c636e4d56b876fdd40de97
Robin <robinkolk@msn.com>
2017-06-04 19:50:47 +0100

    Update copySlicingProfile.js
    this code was unneeded and I forgot to remove, Sorry!

--- host/resourcesnew/cwh/js/copySlicingProfile.js
+++ host/resourcesnew/cwh/js/copySlicingProfile.js
@@ -1,20 +1,9 @@
 (function() {
 	var cwhApp = angular.module('cwhApp');
 
-	cwhApp.controller("copySLicingProfileController", function ($scope, $http, $uibModalInstance, title, sliceData, nameProfile) {
+	cwhApp.controller("copySLicingProfileController", function ($scope, $uibModalInstance, title, sliceData, nameProfile) {
 		$scope.title = title;
 		$scope.sliceData = sliceData;
-		
-		$http.get('/services/machine/serialPorts/list').success(
-				function (data) {
-					$scope.serialPorts = data;
-				});
-		
-		$http.get('/services/machine/graphicsDisplays/list').success(
-				function (data) {
-					$scope.graphicsDisplays = data;
-				});
-
 		$scope.save = function () {
 			$uibModalInstance.close(sliceData);
 		};
