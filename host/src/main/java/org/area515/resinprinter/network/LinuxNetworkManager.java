c2e1a20db5d9ae267447bf0b9526b593636c4779
wgilster <wesgilster@gmail.com>
2018-01-21 13:59:06 -0600

    Disable printflow Ensure that APs are returned in Raspbian Stretch

--- host/src/main/java/org/area515/resinprinter/network/LinuxNetworkManager.java
+++ host/src/main/java/org/area515/resinprinter/network/LinuxNetworkManager.java
@@ -111,7 +111,7 @@ public class LinuxNetworkManager implements NetworkManager {
 	@Override
 	public List<NetInterface> getNetworkInterfaces() {
 		List<NetInterface> ifaces = new ArrayList<NetInterface>();
-		String[] nics = IOUtilities.executeNativeCommand(new String[]{"/bin/sh", "-c", "ifconfig | grep Link | awk '''{ print $1 }'''"}, null);
+		String[] nics = IOUtilities.executeNativeCommand(new String[]{"/bin/sh", "-c", "ip -o link show | awk '''{print substr($2, 1, length($2)-1)}'''"}, null);
 		
 		for (String nicName : nics) {
 			NetInterface netFace = new NetInterface();
